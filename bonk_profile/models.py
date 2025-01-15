f37a6e96b2ae8e0c094c8a50cb05f41bafd108cb
Charlotte Dinsdale <charlottedins01@gmail.com>
2025-01-15 14:49:14 +0000

    create models and add cloudinary url to env.py

--- bonk_profile/models.py
+++ bonk_profile/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class BonkProfile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bonkprofile")
+    profile_pic = CloudinaryField('image', default=)
+    total_games = models.PositiveIntegerField()
+
+class HighScore(models.Model):
+    EASY = 'Easy'
+    HARD = 'Hard'
+    HECK = 'Heck'
+
+    DIFFICULTY_CHOICES = [
+        (EASY, 'Easy'),
+        (Hard, 'Hard'),
+        (Heck, 'Heck'),
+    ]
+
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name="highscore")
+    score = models.PositiveIntegerField()
+    difficulty = models.CharField(max_length=12, choices=DIFFICULTY_CHOICES, default=EASY)
