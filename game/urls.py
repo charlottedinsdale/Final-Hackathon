2420ef98701438af054a2d96a60fc0628c52ffd4
Samuel Sutherland <joesuth13@gmail.com>
2025-01-16 10:51:54 +0000

    add game app

--- game/urls.py
+++ game/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', 
-        views.HomePage.as_view(), name='home'),
+    path('easy/', 
+        views.easy, name='easy'),
     
 ]
