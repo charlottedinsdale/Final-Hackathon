ad9ab88375f7277dce9a9fbb82509101c09f42c3
Adam Swanwick <a.j.swanwick@gmail.com>
2025-01-16 12:28:39 +0000

    add global_leaderboard to views add detail to global_leaderboard.html

--- bonk_profile/urls.py
+++ bonk_profile/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+from .views import LeaderboardView
 
 urlpatterns = [
-    path('', 
-        views.HomePage.as_view(), name='home'),
+    path('',  views.HomePage.as_view(), name='home'),
+    path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
 ]
+    
+
--- bonk_profile/views.py
+++ bonk_profile/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+from .models import HighScore
 
 class HomePage(TemplateView):
     """
@@ -7,4 +9,17 @@ class HomePage(TemplateView):
     """
     template_name = 'index.html'
 
-def view_bonk_profile()
+class LeaderboardView(View):
+    template_name = 'bonk_profile/global_leaderboard.html'
+
+    def get(self, request):
+        # ---Get top 10 scores for each difficulty level ---# 
+        easy_leaderboard = HighScore.objects.filter(difficulty='Easy').order_by('-score')[:10]
+        hard_leaderboard = HighScore.objects.filter(difficulty='Hard').order_by('-score')[:10]
+        heck_leaderboard = HighScore.objects.filter(difficulty='Heck').order_by('-score')[:10]
+
+        return render(request, self.template_name, {
+            'easy_leaderboard': easy_leaderboard,
+            'hard_leaderboard': hard_leaderboard,
+            'heck_leaderboard': heck_leaderboard,
+        })
